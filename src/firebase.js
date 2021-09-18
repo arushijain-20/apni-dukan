36ef03ff0870f429862bdb06733757f0b3b49a5b
arushijain-20 <arushijain0202@gmail.com>
2021-09-18 19:58:48 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB1naORSEi2Sx2GAP2bTYk7fYxNnU1-wAw",
+    authDomain: "clone-88679.firebaseapp.com",
+    projectId: "clone-88679",
+    storageBucket: "clone-88679.appspot.com",
+    messagingSenderId: "937242512467",
+    appId: "1:937242512467:web:6473c0775753a974f20aa6",
+    measurementId: "G-T507P9QEVC"
+  };
